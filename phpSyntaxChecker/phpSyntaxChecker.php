c70f78f6300a2e5020e036cef01fd069194a785d
Gonzalo López <lopezgonzalez@hotmail.com>
2021-07-14 10:28:54 +0200

    Update phpSyntaxChecker.php

--- phpSyntaxChecker/phpSyntaxChecker.php
+++ phpSyntaxChecker/phpSyntaxChecker.php
@@ -2,60 +2,56 @@
 
 class phpSyntaxChecker
 {
-      private $systemFunctionIsenabled;
+    private $systemFunctionIsenabled;
 
-      public function __construct()
-      {
-          $this->checkIfSyntaxCheckerCommandCanBeExecuted();
-      }
-      public function checkPhpFileSyntax(array $arrPhpFiles)
-      {
-          foreach ($arrPhpFiles as $file) {
-              $result = 0;
-              $comando = 'php -l ' . $file;
-              system($comando, $result);
-          }
-      }
+    public function __construct()
+    {
+        $this->checkIfSyntaxCheckerCommandCanBeExecuted();
+    }
+    public function checkPhpFileSyntax(array $arrPhpFiles)
+    {
+        foreach ($arrPhpFiles as $file) {
+            $result = 0;
+            $comando = 'php -l ' . $file;
+            system($comando, $result);
+        }
+    }
 
-      private function getPhpFiles(string $dir): array
-      {
+    private function getPhpFiles(string $dir): array
+    {
         return glob("$dir/*.php");
-      }
+    }
 
-      private function checkIfSyntaxCheckerCommandCanBeExecuted(): void
-      {
+    private function checkIfSyntaxCheckerCommandCanBeExecuted(): void
+    {
         $disabledFunctions = explode(',', ini_get('disable_functions'));
-        $this->systemFunctionIsenabled = (in_array('system',$disabledFunctions)) ? false : true;
-      }
+        $this->systemFunctionIsenabled = (in_array('system', $disabledFunctions)) ? false : true;
+    }
 
-      public function analyzePhpFilesSyntax(string $ruta = __DIR__, bool $checkRoot = true)
-      {
-          $dh = opendir($ruta);
-          $arrResult = array();
-          if($this->systemFunctionIsenabled)
-          {
-              while (($file = readdir($dh)) !== false)
-              {
-                  $dir = $ruta.'/'.$file;
-                  if (is_dir($dir) && $file != '.' && $file != '..'){
-                     $arrPhpFiles = $this->getPhpFiles($dir);
-                     $arrResult = $this->checkPhpFileSyntax($arrPhpFiles);
-                     unset($arrPhpFiles);
-                     $this->analyzePhpFilesSyntax($dir,false);
-                  }
-              }
-              closedir($dh);
+    public function analyzePhpFilesSyntax(string $ruta = __DIR__, bool $checkRoot = true)
+    {
+        $dh = opendir($ruta);
+        $arrResult = array();
+        if ($this->systemFunctionIsenabled) {
+            while (($file = readdir($dh)) !== false) {
+                $dir = $ruta.'/'.$file;
+                if (is_dir($dir) && $file != '.' && $file != '..') {
+                    $arrPhpFiles = $this->getPhpFiles($dir);
+                    $arrResult = $this->checkPhpFileSyntax($arrPhpFiles);
+                    unset($arrPhpFiles);
+                    $this->analyzePhpFilesSyntax($dir, false);
+                }
+            }
+            closedir($dh);
 
-              if ($checkRoot){
-                  $arrPhpFiles = $this->getPhpFiles(__DIR__);
-                  $arrResult = $this->checkPhpFileSyntax($arrPhpFiles);
-             }
+            if ($checkRoot) {
+                $arrPhpFiles = $this->getPhpFiles(__DIR__);
+                $arrResult = $this->checkPhpFileSyntax($arrPhpFiles);
+            }
+        } else {
+            echo 'It is necessary to enable system function in php.ini';
         }
-        else{
-          echo 'It is necessary to enable system function in php.ini';
-        }
-
-      }
+    }
 }
 
 $syntaxAnalyser = new phpSyntaxChecker();
